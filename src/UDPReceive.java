d6428c0d8fbcba19b829161164f2fdb61e6bcf1a
Saabertooth1 <nalas3@icloud.com>
2017-11-24 16:27:32 +0100

    1

--- src/UDPReceive.java
+++ src/UDPReceive.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
@@ -5,6 +7,7 @@ public class UDPReceive {
     public static void main(String args[]) {
         try {
             int port = 8000;
+            Robot robot = new Robot();
 
             // Create a socket to listen on the port.
             DatagramSocket dsocket = new DatagramSocket(port);
@@ -25,6 +28,80 @@ public class UDPReceive {
                 // Convert the contents to a string, and display them
                 String msg = new String(buffer, 0, packet.getLength());
                 System.out.println(packet.getAddress().getHostName() + ": " + msg);
+
+                switch (msg){
+                    case "pausa":
+                        robot.keyPress(KeyEvent.VK_SPACE);
+                        robot.keyRelease(KeyEvent.VK_SPACE);
+                        break;
+
+                    case "+5s":
+                        robot.keyPress(KeyEvent.VK_RIGHT);
+                        robot.keyRelease(KeyEvent.VK_RIGHT);
+                        robot.keyPress(KeyEvent.VK_CONTROL);
+                        robot.keyPress(KeyEvent.VK_RIGHT);
+                        robot.keyRelease(KeyEvent.VK_RIGHT);
+                        robot.keyRelease(KeyEvent.VK_CONTROL);
+                        break;
+
+                    case "-5s":
+                        robot.keyPress(KeyEvent.VK_LEFT);
+                        robot.keyRelease(KeyEvent.VK_LEFT);
+                        break;
+
+                    case "m":
+                        robot.keyPress(KeyEvent.VK_M);
+                        robot.keyRelease(KeyEvent.VK_M);
+                        robot.keyPress(KeyEvent.VK_ALT_GRAPH);
+                        robot.keyPress(KeyEvent.VK_SHIFT);
+                        robot.keyPress(KeyEvent.VK_DOWN);
+                        robot.keyRelease(KeyEvent.VK_DOWN);
+                        robot.keyRelease(KeyEvent.VK_SHIFT);
+                        robot.keyRelease(KeyEvent.VK_ALT_GRAPH);
+                        break;
+
+                    case "f":
+                        robot.keyPress(KeyEvent.VK_F);
+                        robot.keyRelease(KeyEvent.VK_F);
+                        robot.keyPress(KeyEvent.VK_CONTROL);
+                        robot.keyPress(KeyEvent.VK_F);
+                        robot.keyRelease(KeyEvent.VK_F);
+                        robot.keyRelease(KeyEvent.VK_CONTROL);
+                        break;
+
+                    case "up":
+                        robot.keyPress(KeyEvent.VK_UP);
+                        robot.keyRelease(KeyEvent.VK_UP);
+                        robot.keyPress(KeyEvent.VK_CONTROL);
+                        robot.keyPress(KeyEvent.VK_UP);
+                        robot.keyRelease(KeyEvent.VK_UP);
+                        robot.keyRelease(KeyEvent.VK_CONTROL);
+                        break;
+
+                    case "down":
+                        robot.keyPress(KeyEvent.VK_DOWN);
+                        robot.keyRelease(KeyEvent.VK_DOWN);
+                        break;
+
+                    case "0":
+                        robot.keyPress(KeyEvent.VK_0);
+                        robot.keyRelease(KeyEvent.VK_0);
+                        break;
+                    case "nueva lista":
+                        robot.keyPress(0x8);
+                        robot.keyPress(KeyEvent.VK_N);
+                        robot.keyRelease(KeyEvent.VK_N);
+                        robot.keyRelease(0x8);
+
+                        /**robot.keyPress(KeyEvent.VK_A);
+                        robot.keyRelease(KeyEvent.VK_A);
+                        robot.keyPress(KeyEvent.VK_N);
+                        robot.keyRelease(KeyEvent.VK_N);**/
+
+                }
+
+
+
 
                 // Reset the length of the packet before reusing it.
                 packet.setLength(buffer.length);
